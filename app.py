3d26cc22a4d49be26216756382cc3654fc1cbada
AGDNaMiare <agdnamiare@gmail.com>
2025-04-01 21:48:48 +0200

    Kompletna poprawka wszystkich plików

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 import os
 
 # Inicjalizacja rozszerzeń
 db = SQLAlchemy()
 migrate = Migrate()
-login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -18,22 +16,21 @@ def create_app():
     # Inicjalizacja rozszerzeń z aplikacją
     db.init_app(app)
     migrate.init_app(app, db)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
-    
-    # Rejestracja blueprintów (możesz dodać później)
-    # from .auth import auth_blueprint
-    # app.register_blueprint(auth_blueprint)
     
     # Import modeli (musi być po inicjalizacji db)
-    from models.user import User
-    from models.match import Match
-    from models.prediction import Prediction
+    with app.app_context():
+        from models.user import User
+        from models.match import Match
+        from models.prediction import Prediction
     
-    # Prosta trasa testowa
+    # Proste trasy testowe
     @app.route('/')
     def home():
         return "Aplikacja do typowania wyników piłkarskich działa poprawnie!"
+    
+    @app.route('/healthcheck')
+    def healthcheck():
+        return "OK", 200
     
     return app
 
