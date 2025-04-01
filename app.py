aec5d2da322666e4ebc104239e83c52718f82ab6
AGDNaMiare <agdnamiare@gmail.com>
2025-04-01 21:30:47 +0200

    Dodano konfigurację dla zmiennych środowiskowych

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 import os
+from dotenv import load_dotenv
 
 migrate = Migrate(app, db)
 app = Flask(__name__)
 app.config.from_object('config.Config')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
