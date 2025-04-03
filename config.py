3cd4330f05785d45009488d027ed3f39b949a6b6
AGDNaMiare <agdnamiare@gmail.com>
2025-04-03 09:08:21 +0200

    Poprawione szablony i routing

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key-123456')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', '').replace('postgres://', 'postgresql://') or 'postgresql:///deeptype'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'tajny-klucz-123')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    API_FOOTBALL_KEY = os.getenv('API_FOOTBALL_KEY')
-    API_FOOTBALL_URL = 'https://v3.football.api-sports.io'
